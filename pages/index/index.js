4a9c5ab6d856331ca036729bab8746cb794f351d
Jin.Zhang <447718638@qq.com>
2023-03-20 16:43:46 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 Page({
   onReady() {
     this.mainColor = this.selectComponent('#mainColor')
-    this.mainColor.get('../../images/205164915.jpg').then(rgb => {
+    this.mainColor.get('/images/205164915.jpg').then(rgb => {
       console.log(rgb)
       this.setData({
         rgb
